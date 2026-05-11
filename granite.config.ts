560f1cfcd45534c380e87a934f482c25484d0c51
key7536-png <key7536@gmail.com>
2026-05-11 20:09:02 +0900

    Update granite.config.ts

--- granite.config.ts
+++ granite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from '@granite-js/react-native'; 
-export default defineConfig({ 
-  appName: '¶°³ª¿ä', 
-  scheme: 'find-my-fare', 
-  icon: './public/app-icon.png', 
-}); 
+import { defineConfig } from '@granite-js/react-native';
+
+export default defineConfig({
+  appName: 'tteonayo',
+  scheme: 'find-my-fare',
+  icon: './public/app-icon.png',
+});
